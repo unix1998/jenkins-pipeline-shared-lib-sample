868925714c0d448e5112ce8a2ffcbda071668430
A M X <34556710+unix1998@users.noreply.github.com>
2020-09-06 21:09:37 -0400

    Update SampleDSL.groovy

--- vars/SampleDSL.groovy
+++ vars/SampleDSL.groovy
@@ -1,29 +1,46 @@
 // vars/sampleDSL.groovy
 def call(body) {
-    def config = [:]
+    def jenkins_config = [:]
     body.resolveStrategy = Closure.DELEGATE_FIRST
-    body.delegate = config
+    body.delegate = jenkins_config
     body()
 
-   node ('master') {
-      //  agent { label 'build-server' }
-
-       
-            stage('Initialization') {
-                steps {
-                    
-                       echo "initialization"
-                   
-                }
-            }
-            stage('CI') {
-                
-                    echo ('test 2020 Sep sep')
-             
-            }
-            stage('Service') {
-            echo "Start docker service"
-          }
-        }
-    
+    node {
+     stage ('hardware info'){
+        echo ' step1'
+        echo '################'
+        echo jenkins_config.HW
+         
+        sh '''
+        echo "GIT_PREVIOUS_SUCCESSFUL_COMMIT" : $GIT_PREVIOUS_SUCCESSFUL_COMMIT
+        echo "GIT_PREVIOUS_SUCCESSFUL_COMMIT" : $GIT_PREVIOUS_SUCCESSFUL_COMMIT
+        echo $GIT_PREVIOUS_SUCCESSFUL_COMMIT
+        #echo ${env.BRANCH_NAME}
+        #echo ${env.GIT_PREVIOUS_COMMIT}
+        echo $JOB_URL
+        '''
+        //def since1 = System.getenv('GIT_PREVIOUS_SUCCESSFUL_COMMIT') 
+         def since1 = env.GIT_PREVIOUS_SUCCESSFUL_COMMIT
+        println ('GIT_PREVIOUS_SUCCESSFUL_COMMIT')
+        println(since1)
+        println(env.GIT_PREVIOUS_SUCCESSFUL_COMMIT)
+      }
+     stage ('config info') {
+        echo 'stage 2'
+        echo jenkins_config.P1
+        echo "stage 3 , March"
+        def method11 = new Method1()
+        method11.power2(2,3)
+      }
+     stage ('3rd stage ') {
+        echo "stage 3"
+            
+        echo jenkins_config.Custom
+        echo jenkins_config.Guest_name
+        echo "before Guest"
+        aa = math1.power1(3,3)
+        println aa
+        sayHello jenkins_config.Guest_name
+      }
+    }
 }
