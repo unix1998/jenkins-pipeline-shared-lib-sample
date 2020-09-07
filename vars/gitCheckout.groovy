ef0209312c22798d98b33807f3cf614b705d1278
A M X <34556710+unix1998@users.noreply.github.com>
2020-09-06 20:48:02 -0400

    Update gitCheckout.groovy

--- vars/gitCheckout.groovy
+++ vars/gitCheckout.groovy
@@ -6,10 +6,10 @@ def call(body) {
 
     echo 'Checking out code from "' + config.repoUrl + '" with credentialsId "' + \
         config.credentialsId + '" ...'
-    
-    checkout([$class: 'GitSCM', branches: [[name: config.branches]], 
-    doGenerateSubmoduleConfigurations: false, 
-    extensions: [], 
-    submoduleCfg: [], 
-    userRemoteConfigs: [[credentialsId: config.credentialsId, url: config.repoUrl]]])   
+    echo 'test 1234'
+    //checkout([$class: 'GitSCM', branches: [[name: config.branches]], 
+   // doGenerateSubmoduleConfigurations: false, 
+  //  extensions: [], 
+  //  submoduleCfg: [], 
+  //  userRemoteConfigs: [[credentialsId: config.credentialsId, url: config.repoUrl]]])   
 }
